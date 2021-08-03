d26144227463ca646a3b36f600af1ec305cacc59
Matt Elstone <matthew_elstone@hotmail.com>
2021-08-03 12:31:55 +0000

    added github api

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,3 +1,21 @@
+function userInformationHTML(user){
+    return`
+    <h2>${user.name}
+    <span class="small-name">
+    (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+    </span>
+    </h2>
+    <div class="gh-content">
+        <div class="gh-avatar">
+            <a href="${user.html_url}" target="_blank">
+                <img src="${user.avatar_url}" width="80" height="80" alt"${user.login}"/>
+            </a> 
+        </div>
+        <p>Followers: ${user.followers} - Following: ${user.following} <br>
+        Repos: ${user.public_repos}</p>    
+    </div> `;
+}
+
 function fetchGitHubInformation(event){
 
     var username = $("#gh-username").val();
@@ -6,10 +24,13 @@ function fetchGitHubInformation(event){
         return;
     }
 
-    $("#gh-user-data").html(`<div id="loader"> <img src="assets/css/loader.gif" alt="loading"/> </div>`);
+    $("#gh-user-data").html(
+        `<div id="loader">
+            <img src="assets/css/loader.gif" alt="loading..." />
+        </div>`);
 
     $.when(
-        $getJSON(`https://api.github.com/users/${username}`)
+        $.getJSON(`https://api.github.com/users/${username}`)
     ).then(
         function(response) {
             var userData = response;
@@ -18,11 +39,11 @@ function fetchGitHubInformation(event){
         function(errorResponse) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
-                    `<h2>No info found for user ${username}</h2>`
-                );
+                    `<h2>No info found for user ${username}</h2>`);
             } else {
                 console.log(errorResponse);
-                $("#gh-user-data").html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
             }
         });
 }
